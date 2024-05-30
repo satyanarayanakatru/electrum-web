edd847d97270a935ea57dc879f51ce370919645b
nani <satyanarayanayadav50@gmail.com>
2024-05-30 19:21:10 +0530

    changes

--- src/app/blog/ev-charging-energy-blog/layout.jsx
+++ src/app/blog/ev-charging-energy-blog/layout.jsx
@@ -1,13 +1,9 @@
-// import Navbar from "../../../moderncomponents/navbar";
-// import Footer from "@/moderncomponents/footer";
 
 export default function CarbonCreditLayout({ children }) {
   return (
     <div>
       {" "}
-      {/* <Navbar /> */}
       {children}
-      {/* <Footer /> */}
     </div>
   );
 }
